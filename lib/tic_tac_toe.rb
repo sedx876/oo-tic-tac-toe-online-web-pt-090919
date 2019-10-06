6794c1ea2c86499199b33f94230c3e2ffe2d95b9
Sharon Watkins <swatkins876@gmail.com>
2019-10-06 16:28:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,16 +96,7 @@ end
     end
   end
 
-  def play
-    while over? == false
-    turn
-  end
-  if won?
-    puts "Congratulations #{winner}!"
-  elsif draw?
-    puts "Cat's Game!"
-  end
-end
+  
 
 end 
 
